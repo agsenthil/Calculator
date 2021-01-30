3bde5f7d31b3546c89fa40728880d81bf7b6e386
visa <visa_win@yahoo.co.in>
2021-01-29 21:10:28 -0600

    Update Calculator.java
    Refactored

--- src/main/java/com/learning/idea/Calculator.java
+++ src/main/java/com/learning/idea/Calculator.java
@@ -6,25 +6,25 @@ import static com.learning.idea.util.MathMethods.*;
 
 public class Calculator {
     public static void main(String[] args) {
-        String s1 = getInput("Enter a numeric value: ");
-        String s2 = getInput("Enter a numeric value: ");
-        String op = getInput("Choose an operation (+ - * /):");
+        String numeric1 = getInput("Enter a numeric value: ");
+        String numeric2 = getInput("Enter a numeric value: ");
+        String operation = getInput("Choose an operation (+ - * /):");
 
         double result;
 
         try {
-            switch (op) {
+            switch (operation) {
                 case "+":
-                    result = addValues(s1, s2);
+                    result = addValues(numeric1, numeric2);
                     break;
                 case "-":
-                    result = subtractValues(s1, s2);
+                    result = subtractValues(numeric1, numeric2);
                     break;
                 case "*":
-                    result = multiplyValues(s1, s2);
+                    result = multiplyValues(numeric1, numeric2);
                     break;
                 case "/":
-                    result = divideValues(s1, s2);
+                    result = divideValues(numeric1, numeric2);
                     break;
                 default:
                     System.out.println("Unrecognized operation!");
